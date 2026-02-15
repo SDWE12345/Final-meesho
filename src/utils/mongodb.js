f658adf60fd29e026d3ca69eaffb13607600bd4f
SDWE12345 <xx5190443@gmail.com>
2026-02-15 17:59:52 +0530

    Update mongodb.js

--- src/utils/mongodb.js
+++ src/utils/mongodb.js
@@ -1,11 +1,8 @@
 // utils/mongodb.js
 import mongoose from 'mongoose';
 
-const MONGODB_URI = process.env.MONGODB_URI || 'mongodb+srv://v4x123:v4x123@cluster0.i3hnzcs.mongodb.net/meesho';
+const MONGODB_URI =  'mongodb+srv://v4x123:v4x123@cluster0.i3hnzcs.mongodb.net/meesho';
 
-if (!MONGODB_URI) {
-  throw new Error('Please define the MONGODB_URI environment variable');
-}
 
 let cached = global.mongoose;
 
