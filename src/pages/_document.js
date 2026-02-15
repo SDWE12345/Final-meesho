79aa2b469a5ab39d32d2b4f87ae4b9ee44c53e43
SDWE12345 <xx5190443@gmail.com>
2026-02-16 02:04:46 +0530

    Update _document.js

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -1,6 +1,5 @@
 // pages/_document.js
 import { Html, Head, Main, NextScript } from 'next/document';
-import Script from 'next/script';
 
 export default function Document() {
   return (
@@ -8,38 +7,6 @@ export default function Document() {
       <Head>
         <meta charSet="utf-8" />
         <link rel="icon" href="/favicon.ico" />
-
-        {/* Facebook Pixel Code */}
-        <>
-          <Script
-            id="facebook-pixel"
-            strategy="afterInteractive"
-            dangerouslySetInnerHTML={{
-              __html: `!function(f,b,e,v,n,t,s)
-{if(f.fbq)return;n=f.fbq=function(){n.callMethod?
-n.callMethod.apply(n,arguments):n.queue.push(arguments)};
-if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
-n.queue=[];t=b.createElement(e);t.async=!0;
-t.src=v;s=b.getElementsByTagName(e)[0];
-s.parentNode.insertBefore(t,s)}(window, document,'script',
-'https://connect.facebook.net/en_US/fbevents.js');
-fbq('init', '843013301439535');
-fbq('track', 'PageView');
-
-              `,
-            }}
-          />
-          <noscript>
-            <img
-              height="1"
-              width="1"
-              style={{ display: 'none' }}
-              src={`https://www.facebook.com/tr?id=${"843013301439535"}&ev=PageView&noscript=1`}
-              alt=""
-            />
-          </noscript>
-        </>
-
       </Head>
       <body>
         <Main />
